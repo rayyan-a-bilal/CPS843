7d686503dd19be756ecbeb49bd3c2fdcee828b03
rayyan-bilal <81191059+rayyan-bilal@users.noreply.github.com>
2023-11-24 16:45:02 -0500

    -- adding labellmg -- completed code for image collection for training

--- ASL Subtitle Generator/project.py
+++ ASL Subtitle Generator/project.py
@@ -3,7 +3,7 @@ import os
 import time
 import uuid #to name our files
 
-IMAGES_PATH = "Tensorflow/workspace/images/collectedImages" # where all the images are saved
+IMAGES_PATH = r"C:\Users\rayya\PycharmProjects\CPS843 project\RealTimeObjectDetection\Tensorflow\workspace\images\collectedImages"
 
 labels = ['hello', 'thanks','yes', 'no', 'ily']
 number_images = 15
@@ -11,6 +11,19 @@ number_images = 15
 #Code for collecting images
 
 for label in labels:
-    !mkdir{'Tensorflow/workspace/images/collectedImages\\'+label}
+    ## Label Collection ##
+    os.makedirs(os.path.join(IMAGES_PATH, label), exist_ok=True)
     cap = cv2.VideoCapture(0)
-    print("Collecting ")
+    print("Collecting images for {}", format(label))
+    time.sleep(5)
+    for imageNum in range(number_images):
+        ret,frame = cap.read()
+        imagename = os.path.join(IMAGES_PATH, label, label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imagename, frame)
+        cv2.imshow('frame', frame)
+        time.sleep(2)
+
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
+    cap.release()
+cv2.destroyAllWindows()  # Close all OpenCV windows at the end
